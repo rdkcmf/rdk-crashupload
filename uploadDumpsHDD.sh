51ab00f22fd1eef87b448d598b32f3f94039a0f9
Sandeep Nair <Sandeep_Nair2@cable.comcast.com>
2017-08-18 17:43:35 +0000

    Revert "RDKB-7918 : Secure Connection for Logging/Telemetry"
    This reverts commit 7cb00ade997e40cd773aa9bee45ddb044f67faa9.
    
    Change-Id: Id293fbde96fd83e8bf3338bba2ede8814f083533

--- uploadDumpsHDD.sh
+++ uploadDumpsHDD.sh
@@ -484,7 +484,7 @@ uploadToS3()
     IFS=$'\n'
 
     ##sets positional variables $1, $2... Please don't quote this additionally unless you really know what you are doing
-    set -- `curl -s --tlsv1.2 --cacert "$CERTFILE" --data-urlencode "source=$file"\
+    set -- `curl -s --cacert "$CERTFILE" --data-urlencode "source=$file"\
                                          --data-urlencode "dumptype=core"\
                                          --data-urlencode "mod=$modNum"\
                                          --data-urlencode "app=$app" \
@@ -502,8 +502,15 @@ uploadToS3()
             local auth=`sanitize "$2"`
             local remotePath=`sanitize "$3"`
             logMessage "Safe params: $validDate -- $auth -- $remotePath"
-            logMessage "Attempting TLS1.2 connection to Amazon S3"
-            nice -n 19 curl --tlsv1.2 --cacert "$CERTFILE" -X PUT -T "$WORKING_DIR/$file" -H "Host: $S3BUCKET.s3.amazonaws.com" -H "Date: $validDate" -H "Content-Type: application/x-compressed-tar" -H "Authorization: AWS $auth" "https://$S3BUCKET.s3.amazonaws.com/${remotePath}" |logStdout
+            tlsMessage=""
+            if [ -f /etc/os-release ]; then
+                tlsMessage="with TLS1.2"
+                logMessage "Attempting TLS1.2 connection to Amazon S3"
+                nice -n 19 curl --tlsv1.2 --cacert "$CERTFILE" -X PUT -T "$WORKING_DIR/$file" -H "Host: $S3BUCKET.s3.amazonaws.com" -H "Date: $validDate" -H "Content-Type: application/x-compressed-tar" -H "Authorization: AWS $auth" "https://$S3BUCKET.s3.amazonaws.com/${remotePath}" |logStdout
+            else
+                nice -n 19 curl --cacert "$CERTFILE" -X PUT -T "$WORKING_DIR/$file" -H "Host: $S3BUCKET.s3.amazonaws.com" -H "Date: $validDate" -H "Content-Type: application/x-compressed-tar" -H "Authorization: AWS $auth" "https://$S3BUCKET.s3.amazonaws.com/${remotePath}" |logStdout
+
+            fi
             ec=$?
          fi
      fi
@@ -511,7 +518,7 @@ uploadToS3()
      if [ $ec -ne 0 ]; then
          logMessage "Curl finished unsuccessfully! Error code: $ec"
      else
-        logMessage "S3 Log Upload is successful with TLS1.2"
+        logMessage "S3 Log Upload is successful $tlsMessage"
      fi
 
     return $ec
