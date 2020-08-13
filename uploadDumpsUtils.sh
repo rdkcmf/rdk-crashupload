7ed982d99185b1e6968352debfca4aec0736dc10
Jayanth Rajan <jayanth.rajan@sky.uk>
2020-08-13 14:27:29 +0100

    SKYH4-1678 : Devices will not be able to access servers
    Reason for change: Made changes in following two scripts which was pushed under SKYH4-1805
    1)uploadDumpsUtils.sh
    2)uploadDumpsUtilsBroadband.sh
    Test Procedure: Check if crash upload is working properly.
    Risks: Low
    
    Signed-off-by: Jayanth Rajan <jayanth.rajan@sky.uk>
    (cherry picked from commit d2cd5ca5e9a90dd82e5707be83e36936c54f488a)
    (cherry picked from commit c90c497ba5d9d1ac9dabe9fbcca39a2a4cd9dbb9)
    Change-Id: I515d3efe1536e1fd7a6229ecee897dfaf0573907

--- uploadDumpsUtils.sh
+++ uploadDumpsUtils.sh
@@ -52,7 +52,7 @@ getIPAddress()
 if [ "x$BOX_TYPE" = "xHUB4" ]; then
     CURRENT_WAN_IPV6_STATUS=`sysevent get ipv6_connection_state`
     if [ "xup" = "x$CURRENT_WAN_IPV6_STATUS" ] ; then
-            wanIP=`ifconfig $HUB4_IPV6_INTERFACE | grep inet6 | grep -i 'Global'`
+            wanIP=`ifconfig $HUB4_IPV6_INTERFACE | grep Global |  awk '/inet6/{print $3}' | cut -d '/' -f1 | head -n1`
     else
             wanIP=`ifconfig $WANINTERFACE | grep "inet addr" | grep -v inet6 | cut -f2 -d: | cut -f1 -d" "`
     fi
