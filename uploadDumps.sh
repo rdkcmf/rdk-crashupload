b8dac9d5d9b2b78911587ca1ce4964217b2a4667
Sergei Danilchuk <sergei_danilchuk@cable.comcast.com>
2021-04-08 17:56:49 +0000

    RDK-31220 : Modify listed bootup scripts to use ash, not bash 2
    Reason for change:
    Improve bootup performance.
    
    Test Procedure:
    All listed in the ticket bootup scripts should use ash shebang or be
    invoked from other bootup scripts/services via /bin/busybox sh <script_name>.
    Risks: High.
    Signed-off-by: Sergei Danilchuk <sergei_danilchuk@cable.comcast.com>
    
    (cherry picked from commit b65c43c8f7ca6d91c981424172499cf9509df015)
    Change-Id: Ia11f8d3ec5c1c3e15e08a7e84a77aa0d5e90e66f

--- uploadDumps.sh
+++ uploadDumps.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/busybox sh
 ##########################################################################
 # If not stated otherwise in this file or this component's Licenses.txt
 # file the following copyright and licenses apply:
